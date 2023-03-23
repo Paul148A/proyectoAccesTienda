7b048b7a5278a84927c6e8c04effc6b9064e2b15
Paul Ponce <pap.ponce@yavirac.edu.ec>
2023-03-23 13:42:16 -0500

    changes pp

--- customer/my_account.php
+++ customer/my_account.php
@@ -2,17 +2,15 @@
 
 session_start();
 
-if(!isset($_SESSION['customer_email'])){
+if (!isset($_SESSION['customer_email'])) {
 
-echo "<script>window.open('../checkout.php','_self')</script>";
+  echo "<script>window.open('../checkout.php','_self')</script>";
+} else {
 
-
-}else {
-
-include("includes/db.php");
-include("../includes/header.php");
-include("functions/functions.php");
-include("includes/main.php");
+  include("includes/db.php");
+  include("../includes/header.php");
+  include("functions/functions.php");
+  include("includes/main.php");
 
 
 ?>
@@ -27,78 +25,77 @@ include("includes/main.php");
     </div>
   </main>
 
-<div id="content" ><!-- content Starts -->
-<div class="container" ><!-- container Starts -->
+  <div id="content"><!-- content Starts -->
+    <div class="container"><!-- container Starts -->
 
 
 
-<div class="col-md-12"><!-- col-md-12 Starts -->
+      <div class="col-md-12"><!-- col-md-12 Starts -->
 
-<?php
+        <?php
 
-$c_email = $_SESSION['customer_email'];
+        $c_email = $_SESSION['customer_email'];
 
-$get_customer = "select * from customers where customer_email='$c_email'";
+        $get_customer = "select * from customers where customer_email='$c_email'";
 
-$run_customer = mysqli_query($con,$get_customer);
+        $run_customer = mysqli_query($con, $get_customer);
 
-$row_customer = mysqli_fetch_array($run_customer);
+        $row_customer = mysqli_fetch_array($run_customer);
 
-$customer_confirm_code = $row_customer['customer_confirm_code'];
+        $customer_confirm_code = $row_customer['customer_confirm_code'];
 
-$c_name = $row_customer['customer_name'];
+        $c_name = $row_customer['customer_name'];
 
-if(!empty($customer_confirm_code)){
+        if (!empty($customer_confirm_code)) {
 
-?>
+        ?>
 
-<div class="alert alert-danger"><!-- alert alert-danger Starts -->
+          <div class="alert alert-danger"><!-- alert alert-danger Starts -->
 
-<strong> Advertencia! </strong> Confirme su correo electrónico y si no ha recibido su correo electrónico de confirmación
+            <strong> Advertencia! </strong> Confirme su correo electrónico y si no ha recibido su correo electrónico de confirmación
 
-<a href="my_account.php?send_email" class="alert-link">
+            <a href="my_account.php?send_email" class="alert-link">
 
-Enviar correo electrónico de nuevo
+              Enviar correo electrónico de nuevo
 
-</a>
+            </a>
 
-</div><!-- alert alert-danger Ends -->
+          </div><!-- alert alert-danger Ends -->
 
-<?php } ?>
+        <?php } ?>
 
-</div><!-- col-md-12 Ends -->
+      </div><!-- col-md-12 Ends -->
 
-<div class="col-md-3"><!-- col-md-3 Starts -->
+      <div class="col-md-3"><!-- col-md-3 Starts -->
 
-<?php include("includes/sidebar.php"); ?>
+        <?php include("includes/sidebar.php"); ?>
 
-</div><!-- col-md-3 Ends -->
+      </div><!-- col-md-3 Ends -->
 
-<div class="col-md-9" ><!--- col-md-9 Starts -->
+      <div class="col-md-9"><!--- col-md-9 Starts -->
 
-<div class="box" ><!-- box Starts -->
+        <div class="box"><!-- box Starts -->
 
-<?php
+          <?php
 
-if(isset($_GET[$customer_confirm_code])){
+          if (isset($_GET[$customer_confirm_code])) {
 
-$update_customer = "update customers set customer_confirm_code='' where customer_confirm_code='$customer_confirm_code'";
+            $update_customer = "update customers set customer_confirm_code='' where customer_confirm_code='$customer_confirm_code'";
 
-$run_confirm = mysqli_query($con,$update_customer);
+            $run_confirm = mysqli_query($con, $update_customer);
 
-echo "<script>alert('Your Email Has Been Confirmed')</script>";
+            echo "<script>alert('Your Email Has Been Confirmed')</script>";
 
-echo "<script>window.open('my_account.php?my_orders','_self')</script>";
+            echo "<script>window.open('my_account.php?my_orders','_self')</script>";
+          }
 
-}
+          if (isset($_GET['send_email'])) {
 
-if(isset($_GET['send_email'])){
+            $subject = "Email Confirmation Message";
 
-$subject = "Email Confirmation Message";
+            $from = "sad.ahmed22224@gmail.com";
 
-$from = "sad.ahmed22224@gmail.com";
-
-$message = "
+            $message = "
 
 <h2>
 Email Confirmation By Computerfever.com $c_name
@@ -112,84 +109,77 @@ Click Here To Confirm Email
 
 ";
 
-$headers = "From: $from \r\n";
+            $headers = "From: $from \r\n";
 
-$headers .= "Content-type: text/html\r\n";
+            $headers .= "Content-type: text/html\r\n";
 
-mail($c_email,$subject,$message,$headers);
+            mail($c_email, $subject, $message, $headers);
 
-echo "<script>alert('Your Confirmation Email Has Been sent to you, check your inbox')</script>";
+            echo "<script>alert('Your Confirmation Email Has Been sent to you, check your inbox')</script>";
 
-echo "<script>window.open('my_account.php?my_orders','_self')</script>";
-
-}
-
-
-
-if(isset($_GET['my_orders'])){
-
-include("my_orders.php");
-
-}
-
-if(isset($_GET['pay_offline'])) {
-
-include("pay_offline.php");
-
-}
-
-if(isset($_GET['edit_account'])) {
-
-include("edit_account.php");
-
-}
-
-if(isset($_GET['change_pass'])){
-
-include("change_pass.php");
-
-}
-
-if(isset($_GET['delete_account'])){
-
-include("delete_account.php");
-
-}
-
-if(isset($_GET['my_wishlist'])){
-
-include("my_wishlist.php");
-
-}
-
-if(isset($_GET['delete_wishlist'])){
-
-include("delete_wishlist.php");
-
-}
-
-?>
-
-</div><!-- box Ends -->
-
-
-</div><!--- col-md-9 Ends -->
-
-</div><!-- container Ends -->
-</div><!-- content Ends -->
+            echo "<script>window.open('my_account.php?my_orders','_self')</script>";
+          }
 
 
 
-<?php
+          if (isset($_GET['my_orders'])) {
 
-include("../includes/footer.php");
+            include("my_orders.php");
+          }
 
-?>
+          if (isset($_GET['pay_offline'])) {
 
-<script src="js/jquery.min.js"> </script>
+            include("pay_offline.php");
+          }
 
-<script src="js/bootstrap.min.js"></script>
+          if (isset($_GET['edit_account'])) {
 
-</body>
-</html>
+            include("edit_account.php");
+          }
+
+          if (isset($_GET['change_pass'])) {
+
+            include("change_pass.php");
+          }
+
+          if (isset($_GET['delete_account'])) {
+
+            include("delete_account.php");
+          }
+
+          if (isset($_GET['my_wishlist'])) {
+
+            include("my_wishlist.php");
+          }
+
+          if (isset($_GET['delete_wishlist'])) {
+
+            include("delete_wishlist.php");
+          }
+
+          ?>
+
+        </div><!-- box Ends -->
+
+
+      </div><!--- col-md-9 Ends -->
+
+    </div><!-- container Ends -->
+  </div><!-- content Ends -->
+
+
+
+  <?php
+
+  include("../includes/footer.php");
+
+  ?>
+
+  <script src="js/jquery.min.js"> </script>
+
+  <script src="js/bootstrap.min.js"></script>
+
+  </body>
+
+  </html>
 <?php } ?>
